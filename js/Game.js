a3223e3c8567381008fc521a49b4ba597aa4e237
Dāvis Šķipsna <skipsna.davis@gmail.com>
2020-07-03 14:48:59 +0300

    created all methods and properties to classes

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,36 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+  constructor() {
+    this.missed = 0;
+    this.phrases = [];
+    this.activePhrase = null;
+  }
+  /**
+   * @method - Initialize game
+   */
+  startGame() {}
+
+  /**
+   * @method - Randomly retrieves one of the phrases stored in phrases array
+   */
+  getRandomPhrase() {}
+  /**
+   * @method - It checks to see if the button clicked by the player matches a letter in the phrase, and then directs the game based on a correct or incorrect guess.
+   */
+  handleInteraction() {}
+  /**
+   * @method - Removes life, if guessed letter is incorrect, and updates @property missed of this @class {Game}
+   */
+  removeLife() {}
+  /**
+   * @method - Checks to see if player guessed all letters in active phrase
+   */
+  checkForWin() {}
+  /**
+   * @method - Displays message win or loss
+   */
+  gameOver() {}
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,21 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+  constructor(phrase) {
+    this.phrase = phrase.toLowerCase();
+  }
+  /**
+   * @method - Displays letter placeholders, when game is started
+   */
+  addPhraseToDisplay() {}
+  /**
+   * @method - Checks if selected letter by player is equal to phrase letter
+   */
+  checkLetter() {}
+  /**
+   * @method - Shows matched letters in phrase
+   */
+  showMatchedLetter() {}
+}
